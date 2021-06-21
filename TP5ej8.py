ae1603f37db50e1aa0945f8971b61bbab0974e10
Mariela Carriqueo <marielacq@yahoo.com.ar>
2021-06-21 19:25:23 -0300

    Se vuelve a comitear incorporando la variable valor_rot (valor de rotación), para que sea un valor a ingresar por el usuario.

--- TP5ej8.py
+++ TP5ej8.py
@@ -3,7 +3,7 @@
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
 
-def codificado(texto):
+def codificado(texto, valor_rot):
     """
     Esta función codifica un texto con el método ROT13
     """
@@ -12,11 +12,11 @@ def codificado(texto):
     largo_alfabeto = len(alfabeto)
     for letra in texto:
         valor_letra = ord(letra)
-        rotacion = (valor_letra - 65 + 13) % largo_alfabeto
+        rotacion = (valor_letra - 65 + valor_rot) % largo_alfabeto
         codificacion += alfabeto[rotacion]
     return codificacion
 
-def decodificado(texto):
+def decodificado(texto, valor_rot):
     """
     Esta función decodifica un texto con el método ROT13
     """
@@ -25,13 +25,14 @@ def decodificado(texto):
     largo_alfabeto = len(alfabeto)
     for letra in texto:
         valor_letra = ord(letra)
-        rotacion = (valor_letra - 65 + 13) % largo_alfabeto
+        rotacion = (valor_letra - 65 + valor_rot) % largo_alfabeto
         decodificacion += alfabeto[rotacion]
     return decodificacion
 
 def prueba():
     texto = input('Ingrese texto: ')
-    valor = decodificado(texto)
+    valor_rot = int(input('Ingrese valor de rotación: '))
+    valor = decodificado(texto, valor_rot)
     print(f'{valor}')
     
 
